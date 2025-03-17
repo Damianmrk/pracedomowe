7f510ca6267854a35d127274de048b25afd3bc55
Damianmrk <damianchimkowski@gmail.com>
2025-03-17 21:16:57 +0100

    CV do sprawdzenia
    zrobilem CV w ten sposob, probowalem upiekszyc zdjecie borderem radius ale w tym przypadku rowniez mi nie dziala

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Happy developing ✨')
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta chairset="utf-8"/>
+    <title>CV</title>
+<link rel="stylesheet" href="./src/style.css" />
+</head>
+<body>
